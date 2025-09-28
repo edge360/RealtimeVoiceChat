bce764b895d9e09b1d3c53cd6355181a56c794c2
edge360 <edge360@gmail.com>
2025-09-28 18:30:46 -0400

    Update server.py

--- code/server.py
+++ code/server.py
@@ -28,7 +28,7 @@ from fastapi.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse, Response, FileResponse
 
 USE_SSL = False
-TTS_START_ENGINE = "orpheus"
+TTS_START_ENGINE = "kokoro"
 #orpheus, kokoro, coqui"
 TTS_ORPHEUS_MODEL = "orpheus-3b-0.1-ft-Q8_0-GGUF/orpheus-3b-0.1-ft-q8_0.gguf"
 
